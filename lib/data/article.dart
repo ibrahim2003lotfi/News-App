2d0806a651660c427616297717a4472a00c4bbee
ibrahim2003lotfi <ibrahim2003lotfi@gmail.com>
2025-08-22 14:22:05 +0300

    Update article.dart

--- lib/data/article.dart
+++ lib/data/article.dart
@@ -1,6 +1,6 @@
 
 import 'dart:convert';
-
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:http/http.dart' as http;
 import 'package:news_app/models/article_model.dart';
 
@@ -43,3 +43,4 @@ class ArticleNews {
   }
 
 }
+
